7330615b2cf1b6448d16e967a9281a1ce3a89eec
Pradyumna <pradyumna.j@learner.manipal.edu>
2019-01-22 12:45:35 +0530

    update

--- miRPV.sh
+++ miRPV.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 echo -n "welcome to miRPV"
 
+if [ "$1" == "-h" ]; then
+  echo -e "Usage:\nbash `basename $0` /path/to/fasta.fa"
+  exit 0
+fi
 
+##prints help when miRPV path is not given
+if [ "$1" == "" ]; then
+  echo -e "Usage:\nbash `basename $0` /path/to/fasta.fa"
+  exit 0
+fi
+
+
+miRPV_PATH=$1
