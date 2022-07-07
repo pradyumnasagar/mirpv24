e78384f4a0a57b8bbc29c6c7bb57b77901c3cab1
Pradyumna Jayaram <pradyumna.j.ram@gmail.com>
2022-07-07 12:42:35 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -492,7 +492,7 @@ if [ ! -e "$BUILD/$ALGORITHM_SVM_BUILD_DIR/makefile" ] ; then
 	cd "$BUILD/Algorithm-SVM-0.13"
 	
 	/usr/bin/perl Makefile.PL
-	sed -i[bak] "s/'memcpy'/'wmemcpy'/g" bindings.cpp
+	sed -i[bak] "s/memcpy*/wmemcpy/g" bindings.cpp
 	sudo make 
 	sudo make test
 	sudo make install
